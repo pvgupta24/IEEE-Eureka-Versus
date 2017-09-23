fff55cca81e554f7caabdea4938b123d934fde33
Moksh Jain <mokshjn00@gmail.com>
2017-09-24 03:45:29 +0530

    ui changes

--- app/src/main/java/org/ieeenitk/versus/SplashScreenActivity.java
+++ app/src/main/java/org/ieeenitk/versus/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -16,7 +17,12 @@ public class SplashScreenActivity extends AppCompatActivity {
         findViewById(R.id.button_proceed).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String id = ((EditText)findViewById(R.id.text_input)).getText().toString();
+                if (!(id.equals("slot1") || id.equals("slot2") || id.equals("slot3") || id.equals("slot4"))) {
+                    Toast.makeText(SplashScreenActivity.this, "Invalid League Code!", Toast.LENGTH_LONG).show();
+                    return;
+                }
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 intent.putExtra("LeagueID", id);
                 startActivity(intent);
